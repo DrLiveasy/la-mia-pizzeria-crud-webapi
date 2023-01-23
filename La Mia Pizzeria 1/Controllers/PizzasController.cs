4d5301996f00e9c19ec1f6c646a34403ded12069
DrLiveasy <youssef.saleh.hans@gmail.com>
2023-01-23 15:21:13 +0100

    creato controller api PizzasController

---
+++ La Mia Pizzeria 1/Controllers/PizzasController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace La_Mia_Pizzeria_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+    }
+}
