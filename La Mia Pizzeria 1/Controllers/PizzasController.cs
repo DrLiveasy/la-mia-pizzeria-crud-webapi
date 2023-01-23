52bd4fd8f9832344c4066a0cfae24bfce3033bc2
DrLiveasy <youssef.saleh.hans@gmail.com>
2023-01-23 15:36:06 +0100

    creato un metodo Get() che restituisca la lista delle pizze in json nel browser

--- La Mia Pizzeria 1/Controllers/PizzasController.cs
+++ La Mia Pizzeria 1/Controllers/PizzasController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using La_Mia_Pizzeria_1.DataBase;
+using La_Mia_Pizzeria_1.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace La_Mia_Pizzeria_1.Controllers
@@ -7,5 +9,16 @@ namespace La_Mia_Pizzeria_1.Controllers
     [ApiController]
     public class PizzasController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizzas = db.Pizzas.ToList<Pizza>();
+
+                return Ok(pizzas);
+            }
+                
+        }
     }
 }
